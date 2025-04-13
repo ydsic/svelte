96028ca95071258edcdacb32979ae14de7001005
AIR <yd@MacBook-Air.local>
2025-04-14 01:44:18 +0900

    feat: Svelte 틀 push

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../app.css";
+</script>
+
+<h1>Welcome to SvelteKit !!!!</h1>
+<h1 class="text-4xl font-bold text-red-500">Tailwind 정상 작동 중!</h1>
+
+<p>
+  Visit
+  <a class="bg-blue-500" href="https://svelte.dev/docs/kit"
+    >svelte.dev/docs/kit
+  </a>
+  to read the documentation
+</p>
